bc4bbe737c3e6f6d5fb9e9e53dd5b7289b9a6866
LuisQc <pqr.24.08.200@gmail.com>
2022-07-08 11:34:48 -0500

    async-await

--- manejos-de-archivos.js
+++ manejos-de-archivos.js
@@ -1,25 +1,77 @@
-const fs = require("fs");
+const fs = require('fs');
 
-
-fs.readFile("./files/archivo1.txt", "utf-8", (err, data) => {
-     if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-});
-
-console.log("Hola");
-
-fs.rmdir("./carpeta", err=> {
-    if (err) {         console.log(err);
+module.exports = class Container {
+  constructor(filename) {
+    this.filename = filename
   }
-});
 
-fs.readdir("./", (err, files) => {
-    if (err) {
-        throw new Error("No se pudo leer");
-    } else {
-        console.log(files);
+  // Se agrega un producto... Si no existe el archivo, se crea... Si existe, se agrega al array... Se retorna el id del producto...
+  save = async (object) => {
+    try {
+      let objects = await this.getAll()
+        .then(objects => { return objects })
+        .catch(error => { return error })
+      // Se usa reduce para determinar el mayor id... Y se le suma 1 para que sea el nuevo id...
+      object.id = objects.reduce((max, obj) => { return obj.id > max ? obj.id : max }, 0) + 1
+      objects.push(object)
+      await fs.promises.writeFile(this.filename, JSON.stringify(objects))
+      return object.id
+    } catch (error) {
+      console.error(error)
     }
-})
+  }
+
+  // Se obtienen todos los productos...
+  getAll = async () => {
+    try {
+        const objects = await fs.promises.readFile(this.filename, 'utf8')
+        if (objects) {
+          return JSON.parse(objects)
+        } else {
+          // Si no hay productos, se crea un array vacio...
+          return []
+        }
+    } catch (error) {
+      switch (error.code) {
+        case 'ENOENT':
+          // Si no existe el archivo, se devuelve un array vacío...
+          return []
+        default:
+          console.error(error)
+          break
+      }
+    }
+  }
+
+  // Se obtiene el producto con id...
+  getById = async (id) => {
+    try {
+        const object = await this.getAll().then(objects => { return objects.filter(obj => obj.id === id) })
+        return object
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  // Se elimina el producto con id...
+  deleteById = async (id) => {
+    try {
+      const objects = await this.getAll().then(data => {
+        return data.filter(obj => obj.id !== id)
+      })
+      await fs.promises.writeFile(this.filename, JSON.stringify(objects))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  // Se eliminan todos los productos...
+  deleteAll = async () => {
+    try {
+        await fs.promises.writeFile(this.filename, '[]')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+}
